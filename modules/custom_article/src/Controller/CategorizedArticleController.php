9f45bab844f3927e727b4fec8a4dfe65cce9aeac
rahul-gore-asentech <rahul.gore@asentech.com>
2017-11-22 04:35:55 +0530

    link code updated

--- modules/custom_article/src/Controller/CategorizedArticleController.php
+++ modules/custom_article/src/Controller/CategorizedArticleController.php
@@ -26,29 +26,30 @@ class CategorizedArticleController extends ControllerBase {
     //return $category;
     $term = Term::load($category);
     $name = $term->getName();
-    //print($term->url());
     $tlink = $term->url();
     $host = \Drupal::request()->getHost();
     global $base_url;
-    //print($base_url);
+
     $catlink = 'http://'.$host.$tlink;
-    //print '<pre>';print_r($term);print '</pre>';
+
+    /*
     $aliasManager = \Drupal::service('path.alias_manager');
     // The second argument to getAliasByPath is a language code such as "en" or LanguageInterface::DEFAULT_LANGUAGE.
     $more_url = $aliasManager->getAliasByPath('/taxonomy/term/' . $category);
-
-    /*
-    $url = Url::fromRoute('<front>', [], ['attributes' => ['class' => ['foo', 'bar']]]);
-
-    $url->setUrlGenerator($this->urlGenerator);
-
-    $link = Link::fromTextAndUrl('text', $url)->toString();
     /**/
 
+    //$url = Url::fromRoute('<front>', [], ['attributes' => ['class' => ['foo', 'bar']]]);
+    $url = Url::fromUri($catlink);
+    $url->setUrlGenerator($this->urlGenerator);
+
+    $link = Link::fromTextAndUrl('More '.$name, $url)->toString();
+    /**/
+
+    /*
     $url2 = Url::fromUri($catlink);
     $external_link = \Drupal::l(t('More '.$name), $url2);
-
-    print($external_link);
+    /**/
+    //print($link);
 
   //$internal_link = \Drupal::l(t('Book admin'), $url);
 
@@ -57,7 +58,7 @@ class CategorizedArticleController extends ControllerBase {
     return [
       '#theme' => 'categorized_article_main',
       '#category_name' => $name,
-      '#category_linked' => $external_link,
+      '#category_linked' => $link,
       '#categorized_article_listing' => 'Listing here',
       'variables' => ['category_name' => 'cat2', 'category_linked' => 'More Cat2', 'categorized_article_listing' => 'Listing here' ],
       //'#test_var' => t('Test Value'),
