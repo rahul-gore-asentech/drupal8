0b7be0931049a8e2be787985705b3d46027970d9
rahul-gore-asentech <rahul.gore@asentech.com>
2017-11-23 04:24:38 +0530

    entity query added

--- modules/custom_article/src/Controller/CategorizedArticleController.php
+++ modules/custom_article/src/Controller/CategorizedArticleController.php
@@ -17,51 +17,66 @@ use Drupal\Core\Link;
 use Drupal\Core\Url;
 use Drupal\Core\Utility\LinkGenerator;
 use Drupal\Core\GeneratedLink;
+use Drupal\Core\Entity\Query;
 
 class CategorizedArticleController extends ControllerBase {
 
   public function cat_article_main($category) {
 
-    //print_r($category);
-    //return $category;
+
     $term = Term::load($category);
     $name = $term->getName();
     $tlink = $term->url();
+
     $host = \Drupal::request()->getHost();
     global $base_url;
 
     $catlink = 'http://'.$host.$tlink;
 
     /*
+    //get the alias of term
     $aliasManager = \Drupal::service('path.alias_manager');
     // The second argument to getAliasByPath is a language code such as "en" or LanguageInterface::DEFAULT_LANGUAGE.
     $more_url = $aliasManager->getAliasByPath('/taxonomy/term/' . $category);
     /**/
 
+    //creating the Link
     //$url = Url::fromRoute('<front>', [], ['attributes' => ['class' => ['foo', 'bar']]]);
     $url = Url::fromUri($catlink);
     $url->setUrlGenerator($this->urlGenerator);
-
     $link = Link::fromTextAndUrl('More '.$name, $url)->toString();
     /**/
 
     /*
+    //another method to create link
     $url2 = Url::fromUri($catlink);
     $external_link = \Drupal::l(t('More '.$name), $url2);
     /**/
-    //print($link);
-
-  //$internal_link = \Drupal::l(t('Book admin'), $url);
-
-
 
     return [
       '#theme' => 'categorized_article_main',
       '#category_name' => $name,
       '#category_linked' => $link,
-      '#categorized_article_listing' => 'Listing here',
+      '#categorized_article_listing' => 'Listing here...',
       'variables' => ['category_name' => 'cat2', 'category_linked' => 'More Cat2', 'categorized_article_listing' => 'Listing here' ],
       //'#test_var' => t('Test Value'),
     ];/**/
+  }
+
+  public function cat_article_listing($category) {
+
+    $query = \Drupal::entityQuery('node');
+    $query->condition('type', 'news_article');
+    $query->condition('status', 1);
+    $query->condition('field_news_articles_terms', $category);
+    $query->sort('field_post_date', DESC);
+    $query->range(0,10);
+    $nids = $query->execute();
+
+    $nodes = entity_load_multiple('node', $nids);
+    foeach($nodes as $node) {
+      //do something
+    }
+
   }
 }
